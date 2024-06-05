47201fa50f3e32e8f51cd8f16ad99112f4da2690
MarchellaAngelina <marchellangelina@gmail.com>
2024-06-05 10:26:29 +0700

    Menyelesaikan Onboarding Page

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:playandpizza/screens/onboarding_page.dart';
 import 'package:playandpizza/screens/login_screen.dart';
 import 'package:playandpizza/screens/pages_layout.dart';
 import 'package:playandpizza/screens/signup_screen.dart';
@@ -11,153 +13,135 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
+  final PageController _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
+    TextStyle topTextStyle = TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.w600,
+      fontFamily: 'Poppins',
+    );
+
+    TextStyle bottomTextStyle = TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      fontFamily: 'Poppins',
+    );
+
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView(
+              controller: _pageController,
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 20.0, top: 20.0),
-                  child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Image.asset(
-                      'assets/Logo_PNP_Light.png',
-                      width: 100,
-                      height: 100,
-                    ),
-                  ),
+                OnboardingPage(
+                  imagePath: 'assets/Onboarding Illustration - 1.png',
+                  texts: [
+                    'Mainkan Ragam Games',
+                    'yang Menarik',
+                    'menangkan dan kumpulkan tiket',
+                    'sebanyak-banyaknya!'
+                  ],
+                  topTextStyle: topTextStyle,
+                  bottomTextStyle: bottomTextStyle,
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 10.0, top: 25.0),
-                  child: Align(
-                    alignment: Alignment.topRight,
-                    child: TextButton(
-                      onPressed: () {
-                        // Sebagai Guest
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => PagesLayout()));
-                      },
-                      child: const Text(
-                        'Lewati',
-                        style: TextStyle(
-                          color: Color(0xFFFF0000),
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                          fontFamily: 'Poppins',
-                        ),
-                      ),
-                    ),
-                  ),
+                OnboardingPage(
+                  imagePath: 'assets/Onboarding Illustration - 2.png',
+                  texts: [
+                    'Menangkan Pizza dan',
+                    'Hadiah Gratis',
+                    'tukarkan tiket Anda dengan',
+                    'beragam pilihan pizza dan hadiah'
+                  ],
+                  topTextStyle: topTextStyle,
+                  bottomTextStyle: bottomTextStyle,
+                ),
+                OnboardingPage(
+                  imagePath: 'assets/Onboarding Illustration - 3.png',
+                  texts: [
+                    'Nikmati Pizza Grais',
+                    'sambil Bermain',
+                    'mainkan games dan dapatkan',
+                    'pizza gratis hanya di Play &'
+                  ],
+                  topTextStyle: topTextStyle,
+                  bottomTextStyle: bottomTextStyle,
                 ),
               ],
             ),
-            SizedBox(height: 10),
-            Center(
-              child: Image.asset(
-                'assets/Onboarding Illustration - 1.png',
-                width: 400,
-                height: 400,
+          ),
+          SizedBox(
+            height: 20,
+            child: SmoothPageIndicator(
+              controller: _pageController,
+              count: 3,
+              effect: JumpingDotEffect(
+                activeDotColor: Color(0xFFBD0000),
+                dotColor: Color(0xFFFFC1C1),
+                dotHeight: 10,
+                dotWidth: 10,
               ),
             ),
-            SizedBox(height: 10),
-            Text(
-              'Mainkan Ragam Games',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w600,
-                fontFamily: 'Poppins',
+          ),
+          SizedBox(height: 40),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: ElevatedButton(
+              onPressed: () {
+                // Halaman Registration
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SignupScreen()));
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.all(10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4),
+                  side: BorderSide(width: 1, color: Colors.white),
+                ),
+                backgroundColor: Color(0xFFBD0000),
+              ),
+              child: Text(
+                'Gabung Sekarang',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
               ),
             ),
-            Text(
-              'yang Menarik',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w600,
-                fontFamily: 'Poppins',
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Sudah punya akun?',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black,
+                ),
               ),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'menangkan dan kumpulkan tiket',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w400,
-                fontFamily: 'Poppins',
-              ),
-            ),
-            Text(
-              'sebanyak-banyaknya!',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w400,
-                fontFamily: 'Poppins',
-              ),
-            ),
-            SizedBox(height: 60),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.9,
-              child: ElevatedButton(
+              TextButton(
                 onPressed: () {
-                  // Halaman Registration
+                  // Log In
                   Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => SignupScreen()));
+                      MaterialPageRoute(builder: (context) => LoginScreen()));
                 },
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.all(10),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(4),
-                    side: BorderSide(width: 1, color: Colors.white),
-                  ),
-                  backgroundColor: Color(0xFFBD0000),
-                ),
                 child: Text(
-                  'Gabung Sekarang',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Sudah punya akun?',
+                  'Masuk disini',
                   style: TextStyle(
                     fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.black,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFFBD0000),
                   ),
                 ),
-                TextButton(
-                  onPressed: () {
-                    // Masuk Akun
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => LoginScreen()));
-                  },
-                  child: Text(
-                    'Masuk disini',
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600,
-                      color: Color(0xFFBD0000),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-          ],
-        ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+        ],
       ),
     );
   }
---
+++ lib/screens/onboarding_page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:playandpizza/screens/pages_layout.dart';
+
+class OnboardingPage extends StatelessWidget {
+  final String imagePath;
+  final List<String> texts;
+  final TextStyle topTextStyle;
+  final TextStyle bottomTextStyle;
+
+  const OnboardingPage({
+    required this.imagePath,
+    required this.texts,
+    required this.topTextStyle,
+    required this.bottomTextStyle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, top: 20.0),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Image.asset(
+                    'assets/Logo_PNP_Light.png',
+                    width: 100,
+                    height: 100,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 10.0, top: 25.0),
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: TextButton(
+                    onPressed: () {
+                      // Log In as Guest
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PagesLayout()));
+                    },
+                    child: const Text(
+                      'Lewati',
+                      style: TextStyle(
+                        color: Color(0xFFFF0000),
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          Center(
+            child: Image.asset(
+              imagePath,
+              width: 400,
+              height: 400,
+            ),
+          ),
+          SizedBox(height: 10),
+          ...texts.asMap().entries.map((entry) {
+            int index = entry.key;
+            String text = entry.value;
+            return Text(
+              text,
+              style: index < 2 ? topTextStyle : bottomTextStyle,
+              textAlign: TextAlign.center,
+            );
+          }).toList(),
+          SizedBox(height: 60),
+        ],
+      ),
+    );
+  }
+}
