2d9e5f49e739bc70cffce55353b43286d04d3312
HezekiahIvandi <hezekiahivandi@gmail.com>
2024-06-06 16:04:11 +0700

    Setup koneksi firebase dan flutter

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:playandpizza/screens/onboarding.dart';
 import 'package:playandpizza/screens/signup_screen.dart';
 import 'package:playandpizza/screens/login_screen.dart';
 import 'package:playandpizza/screens/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
