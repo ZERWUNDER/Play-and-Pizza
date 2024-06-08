ff61fd619b2567a05e5fc246f293ad55914e859f
HezekiahIvandi <hezekiahivandi@gmail.com>
2024-06-08 12:05:10 +0700

    Menambahkan auth services (login, logged out, sign up)

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:playandpizza/screens/signup_screen.dart';
 import 'package:playandpizza/screens/login_screen.dart';
 import 'package:playandpizza/screens/home.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
